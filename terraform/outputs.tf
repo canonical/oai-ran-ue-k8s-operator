44c8bf25ab19f4ce1861945e8c6709fbd0f1eb07
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-09-14 17:29:00 -0400

    feat: Expose fiveg_rfsim endpoint in terraform module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,11 @@ output "app_name" {
   value       = juju_application.ue.name
 }
 
+output "fiveg_rfsim_endpoint" {
+  description = "Name of the endpoint used to integrate with the rfsim provider."
+  value       = "fiveg_rfsim"
+}
+
 output "logging_endpoint" {
   description = "Name of the endpoint used to integrate with the Logging provider."
   value       = "logging"
