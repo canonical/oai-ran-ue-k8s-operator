1d668b75baf0a1bb807bcdb1672539f89b5c43f2
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2024-10-01 13:59:18 +0200

    fix: Fixes startup params after updating the DU config (#20)
    Fixes startup params after updating the DU config

--- tests/unit/test_charm_configure.py
+++ tests/unit/test_charm_configure.py
@@ -149,7 +149,7 @@ class TestCharmConfigure(UEFixtures):
                             "ue": {
                                 "startup": "enabled",
                                 "override": "replace",
-                                "command": "/opt/oai-gnb/bin/nr-uesoftmodem -O /tmp/conf/ue.conf --sa --rfsim -r 106 --numerology 1 -C 3619200000 --ssb 516 -E --log_config.global_log_options level,nocolor,time --rfsimulator.serveraddr 1.1.1.1",  # noqa: E501
+                                "command": "/opt/oai-gnb/bin/nr-uesoftmodem -O /tmp/conf/ue.conf --sa --rfsim -r 51 --numerology 1 -C 3309480000 --ssb 238 -E --log_config.global_log_options level,nocolor,time --rfsimulator.serveraddr 1.1.1.1",  # noqa: E501
                                 "environment": {"TZ": "UTC"},
                             }
                         }
