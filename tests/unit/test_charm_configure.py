f65d6d01af5bda9441525fd22232186402ce14c0
gatici <gulsum.atici@canonical.com>
2024-12-11 12:45:59 +0300

    feat: extend fiveg_rfsim_interface
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- tests/unit/test_charm_configure.py
+++ tests/unit/test_charm_configure.py
@@ -6,6 +6,7 @@
 import os
 import tempfile
 
+import pytest
 from ops import testing
 from ops.pebble import Layer
 
@@ -38,7 +39,50 @@ class TestCharmConfigure(UEFixtures):
 
             self.mock_k8s_privileged.patch_statefulset.assert_called_with(container_name="ue")
 
-    def test_given_workload_is_ready_to_be_configured_when_configure_then_ue_config_file_is_generated_and_pushed_to_the_workload_container(  # noqa: E501
+    def test_given_workload_is_ready_to_be_configured_and_sst_is_available_when_configure_then_ue_config_file_is_generated_and_pushed_to_the_workload_container(  # noqa: E501
+        self,
+    ):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_check_output.return_value = b"1.2.3.4"
+            rfsim_relation = testing.Relation(
+                endpoint="fiveg_rfsim",
+                interface="fiveg_rfsim",
+                remote_app_data={
+                    "rfsim_address": "1.1.1.1",
+                    "sst": "1",
+                },
+            )
+            config_mount = testing.Mount(
+                source=temp_dir,
+                location="/tmp/conf",
+            )
+            container = testing.Container(
+                name="ue",
+                can_connect=True,
+                mounts={
+                    "config": config_mount,
+                },
+            )
+            state_in = testing.State(
+                leader=True,
+                relations=[rfsim_relation],
+                containers=[container],
+                model=testing.Model(name="whatever"),
+            )
+
+            self.ctx.run(self.ctx.on.pebble_ready(container), state_in)
+
+            with open(
+                "tests/unit/resources/expected_config_with_default_sd.conf"
+            ) as expected_config_file:
+                expected_config = expected_config_file.read()
+
+            with open(f"{temp_dir}/ue.conf") as generated_config_file:
+                generated_config = generated_config_file.read()
+
+            assert generated_config.strip() == expected_config.strip()
+
+    def test_given_workload_is_ready_to_be_configured_but_sst_is_not_available_when_configure_then_ue_config_file_is_not_generated(  # noqa: E501
         self,
     ):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -70,19 +114,24 @@ class TestCharmConfigure(UEFixtures):
 
             self.ctx.run(self.ctx.on.pebble_ready(container), state_in)
 
-            with open("tests/unit/resources/expected_config.conf") as expected_config_file:
-                expected_config = expected_config_file.read()
-
-            with open(f"{temp_dir}/ue.conf") as generated_config_file:
-                generated_config = generated_config_file.read()
-
-            assert generated_config.strip() == expected_config.strip()
+            with pytest.raises(Exception) as e:
+                with open(f"{temp_dir}/ue.conf") as generated_config_file:
+                    generated_config_file.read()
+            assert "No such file or directory" in str(e.value)
 
     def test_given_ue_config_file_is_up_to_date_when_configure_then_ue_config_file_is_not_pushed_to_the_workload_container(  # noqa: E501
         self,
     ):
         with tempfile.TemporaryDirectory() as temp_dir:
             self.mock_check_output.return_value = b"1.2.3.4"
+            rfsim_relation = testing.Relation(
+                endpoint="fiveg_rfsim",
+                interface="fiveg_rfsim",
+                remote_app_data={
+                    "rfsim_address": "1.1.1.1",
+                    "sst": "1",
+                },
+            )
             config_mount = testing.Mount(
                 source=temp_dir,
                 location="/tmp/conf",
@@ -96,19 +145,119 @@ class TestCharmConfigure(UEFixtures):
             )
             state_in = testing.State(
                 leader=True,
-                relations=[],
+                relations=[rfsim_relation],
                 containers=[container],
                 model=testing.Model(name="whatever"),
             )
-            with open("tests/unit/resources/expected_config.conf") as expected_config_file:
+            with open(
+                "tests/unit/resources/expected_config_with_default_sd.conf"
+            ) as expected_config_file:
                 expected_config = expected_config_file.read().strip()
+
             with open(f"{temp_dir}/ue.conf", "w") as generated_config_file:
                 generated_config_file.write(expected_config)
+
             config_modification_time = os.stat(temp_dir + "/ue.conf").st_mtime
 
             self.ctx.run(self.ctx.on.pebble_ready(container), state_in)
 
             assert os.stat(temp_dir + "/ue.conf").st_mtime == config_modification_time
+
+    def test_given_ue_config_file_is_up_to_date_when_rfsim_relation_changed_then_ue_config_file_is_not_pushed_to_the_workload_container(  # noqa: E501
+        self,
+    ):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_check_output.return_value = b"1.2.3.4"
+            rfsim_relation = testing.Relation(
+                endpoint="fiveg_rfsim",
+                interface="fiveg_rfsim",
+                remote_app_data={
+                    "rfsim_address": "1.1.1.1",
+                    "sst": "1",
+                },
+            )
+            config_mount = testing.Mount(
+                source=temp_dir,
+                location="/tmp/conf",
+            )
+            container = testing.Container(
+                name="ue",
+                can_connect=True,
+                mounts={
+                    "config": config_mount,
+                },
+            )
+            state_in = testing.State(
+                leader=True,
+                relations=[rfsim_relation],
+                containers=[container],
+                model=testing.Model(name="whatever"),
+            )
+            with open(
+                "tests/unit/resources/expected_config_with_default_sd.conf"
+            ) as expected_config_file:
+                expected_config = expected_config_file.read().strip()
+
+            with open(f"{temp_dir}/ue.conf", "w") as generated_config_file:
+                generated_config_file.write(expected_config)
+
+            config_modification_time = os.stat(temp_dir + "/ue.conf").st_mtime
+
+            self.ctx.run(self.ctx.on.relation_changed(rfsim_relation), state_in)
+
+            assert os.stat(temp_dir + "/ue.conf").st_mtime == config_modification_time
+
+    def test_given_ue_config_is_exist_when_rfsim_relation_changed_then_new_ue_config_file_is_pushed_to_the_workload_container(  # noqa: E501
+        self,
+    ):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            self.mock_check_output.return_value = b"1.2.3.4"
+            rfsim_relation = testing.Relation(
+                endpoint="fiveg_rfsim",
+                interface="fiveg_rfsim",
+                remote_app_data={"rfsim_address": "1.1.1.1", "sst": "2", "sd": "12555"},
+            )
+            config_mount = testing.Mount(
+                source=temp_dir,
+                location="/tmp/conf",
+            )
+            container = testing.Container(
+                name="ue",
+                can_connect=True,
+                mounts={
+                    "config": config_mount,
+                },
+            )
+            state_in = testing.State(
+                leader=True,
+                relations=[rfsim_relation],
+                containers=[container],
+                model=testing.Model(name="whatever"),
+            )
+
+            with open(
+                "tests/unit/resources/expected_config_with_default_sd.conf"
+            ) as old_config_file:
+                old_config = old_config_file.read().strip()
+
+            with open(f"{temp_dir}/ue.conf", "w") as generated_config_file:
+                generated_config_file.write(old_config)
+
+            config_modification_time = os.stat(temp_dir + "/ue.conf").st_mtime
+
+            self.ctx.run(self.ctx.on.relation_changed(rfsim_relation), state_in)
+
+            with open(f"{temp_dir}/ue.conf") as updated_config_file:
+                updated_config = updated_config_file.read()
+
+            with open(
+                "tests/unit/resources/expected_config_with_new_sd.conf"
+            ) as expected_config_file:
+                expected_config = expected_config_file.read()
+
+            assert updated_config.strip() == expected_config.strip()
+
+            assert os.stat(temp_dir + "/ue.conf").st_mtime != config_modification_time
 
     def test_given_can_connect_when_configure_then_pebble_layer_is_created(
         self,
@@ -120,6 +269,7 @@ class TestCharmConfigure(UEFixtures):
                 interface="fiveg_rfsim",
                 remote_app_data={
                     "rfsim_address": "1.1.1.1",
+                    "sst": "1",
                 },
             )
             config_mount = testing.Mount(
